fb02859caf05a945ab11028f8a011b8c7563898d
CrispyBuns <crispybuns2003@gmail.com>
2025-07-14 15:01:28 -0500

    Change encounter rates in Viridian Forest and Pokemon Tower

--- data/wild/maps/PokemonTower2F.asm
+++ data/wild/maps/PokemonTower2F.asm
@@ -2,13 +2,13 @@ PokemonTower2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 20, GASTLY
 	db 21, GASTLY
-	db 18, VULPIII
-	db 15, VULPIII
-	db 20, VULPIX
+	db 15, GASTLY
+	db 15, GASTLY
+	db 15, GASTLY
 	db 24, VULPIX
 	db 22, CUBONE
 	db 20, CUBONE
-	db 25, HAUNTER
+	db 15, GASTLY
 	db 25, BALUMBA
 	end_grass_wildmons
 
