7ff7e533d53d3991b34fe0a312362d7bf0a48d04
CrispyBuns <crispybuns2003@gmail.com>
2025-07-17 12:04:12 -0500

    say that RGBDS version can be ethier v0.6.0 or v0.6.1

--- rgbdscheck.asm
+++ rgbdscheck.asm
@@ -2,7 +2,8 @@ MAJOR EQU 0
 MINOR EQU 6
 PATCH EQU 0
 
-WRONG_RGBDS EQUS "fail \"pokered requires rgbds v0.6.0 or newer.\""
+WRONG_RGBDS EQUS "fail \"KEP Requires rgbds v0.6.0 or rgbds v0.6.1 exact"\
+  
 
 IF !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
 	WRONG_RGBDS
