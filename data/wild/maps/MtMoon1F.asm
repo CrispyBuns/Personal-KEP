bf4ad0a869bbf270acb4ff2fa9becbbb5d799a50
CrispyBuns <crispybuns2003@gmail.com>
2025-07-17 10:35:00 -0500

    change encounters

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,13 +1,13 @@
 MtMoon1FWildMons:
-	def_grass_wildmons 7 ; encounter rate
+	def_grass_wildmons 8 ; encounter rate
 	db  8, GEODUDE
-	db  7, GEODUDE
+	db  7, BITTYBAT
 	db  9, GEODUDE
-	db  8, GEODUDE
+	db  8, BITTYBAT
 	db  6, GEODUDE
 	db  9, SANDSHREW
-	db  9, GEODUDE
-	db  8, GEODUDE
+	db  9, BITTYBAT
+	db  8, BITTYBAT
 	db 10, GEODUDE
 	db  8, CLEFAIRY
 	end_grass_wildmons
