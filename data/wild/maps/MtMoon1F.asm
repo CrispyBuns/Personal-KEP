bf5a56a5e7a65c81f0a8c688579a6720c478938a
CrispyBuns <crispybuns2003@gmail.com>
2025-07-17 12:05:57 -0500

    Revert MtMoon1F Changes

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,14 +1,14 @@
 MtMoon1FWildMons:
-	def_grass_wildmons 8 ; encounter rate
-	db  8, GEODUDE
+	def_grass_wildmons 10 ; encounter rate
+	db  8, BITTYBAT
 	db  7, BITTYBAT
-	db  9, GEODUDE
-	db  8, BITTYBAT
-	db  6, GEODUDE
-	db  9, SANDSHREW
 	db  9, BITTYBAT
-	db  8, BITTYBAT
-	db 10, GEODUDE
+	db  8, GEODUDE
+	db  6, BITTYBAT
+	db  9, SANDSHREW
+	db  9, GEODUDE
+	db  8, PARAS
+	db 10, BITTYBAT
 	db  8, CLEFAIRY
 	end_grass_wildmons
 
